c6ca66c813f900a3a11baee3ea2c7279ebbd8611
Chanqun Sung <cksgns93@naver.com>
2021-10-11 21:25:27 +0900

    deploy KryptoBirdz

---
+++ nft-marketplace-starter-kit/src/contracts/KryptoBirdz.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Kryptobird {
+    
+    string private name;
+    string private symbol;
+
+    constructor() {
+        name = 'Kryptobird';
+        symbol = 'KBIRDZ';
+    }
+}
