e2941e5ec52e46db6f18a872ae1c8e10b4fea7a0
Chanqun Sung <cksgns93@naver.com>
2021-10-12 01:38:08 +0900

    nft KryptoBirdz

--- nft-marketplace-starter-kit/src/contracts/KryptoBirdz.sol
+++ nft-marketplace-starter-kit/src/contracts/KryptoBirdz.sol
@@ -1,24 +1,33 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import './ERC721Connector.sol';
 
-contract Kryptobird is ERC721Connector {
+contract KryptoBird is ERC721Connector {
 
-    //array to store our nfts
-    string [] public KryptoBirdz;
-    
-    function mint(string memory _KryptoBird) public {
+    // array to store our nfts
+    string [] public kryptoBirdz;
 
-        // this is deprecated - uint _id = KryptoBirdz.push(_KryptoBird);
+    mapping(string => bool) _kryptoBirdzExists;
 
-        KryptoBirdz.push(_KryptoBird);
-        uint _id = KryptoBirdz.length - 1;
+    function mint(string memory _kryptoBird) public {
 
+        require(!_kryptoBirdzExists[_kryptoBird],
+        'Error - kryptoBird already exists');
+        // this is deprecated - uint _id = KryptoBirdz.push(_kryptoBird);
+        kryptoBirdz.push(_kryptoBird);
+        uint _id = kryptoBirdz.length - 1;
+
+        // .push no longer returns the length but a ref to the added element
         _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+
     }
 
-    constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {
-        
+    constructor() ERC721Connector('KryptoBird','KBIRDZ') {
+
     }
+
 }
